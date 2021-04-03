8f85408179fa9abb2bfe6e865f920760c141f294
Simon Nolte <simonnolte@googlemail.com>
2021-04-03 15:27:02 +0200

    Create spiro_add

--- R/process.R
+++ R/process.R
@@ -11,3 +11,16 @@ spiro_interpolate.internal <- function(y, x) {
   dfinter <- interpol$y
   dfinter
 }
+
+spiro_add <- function(data, weight = NULL) {
+  if (is.null(weight)) weight = attr(data, "info")$weight
+  data$VO2_rel <- data$VO2 / weight
+  data$VCO2_rel <- data$VCO2 / weight
+  data$RER <- data$VCO2 / data$VO2
+  data$RE <- (100/6) * (data$VO2_rel / data$load)
+  for (i in seq_along(data$RE)) {
+    if (is.na(data$RE[[i]])) data$RE[[i]] <- NA
+    else if (data$RE[[i]] >= 1000) data$RE[[i]] <- NA
+  }
+  data
+}
