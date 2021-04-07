85d67bad32e984b4ce378e6c063a5dd2cfd1fbaf
Simon Nolte <simonnolte@googlemail.com>
2021-04-07 17:19:29 +0200

    Create package documentation

---
+++ R/spiro-package.R
@@ -0,0 +1,19 @@
+#' spiro: A package to work with data from spiroergometric testing
+#'
+#' The spiro package enables a fast workflow with raw files from metabolic
+#' carts/spiroergometry systems. It provides simple tools for data import,
+#' processing, summary and visualisation.
+#'
+#' @section Main functions:
+#' \itemize{
+#'   \item Use \code{\link{spiro}} for one-step data import and processing from
+#'     raw metabolic cart files.
+#'   \item Use \code{\link{spiro_summary}} and \code{\link{spiro_glance}} for
+#'     getting a stepwise or overall summary of the imported data.
+#'   \item Use the \code{\link{spiro_plot}} functions as a shortcut for
+#'     visualising the imported data.
+#' }
+#'
+#' @name spiro-package
+#' @docType package
+NULL
