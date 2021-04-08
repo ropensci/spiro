bba9fb37f72bbbb636bd822076c6becc5118f54e
Simon Nolte <simonnolte@googlemail.com>
2021-04-08 21:27:37 +0200

    Add method for better printing of spiro data.frames in Markdown

---
+++ R/knit_print.R
@@ -0,0 +1,34 @@
+#' Printing spiroergometric data frames in a knitr context
+#'
+#' \code{knit_print.spiro()} provides a method for printing \code{data.frames}
+#' from \code{\link{spiro}} within \code{knitr}.
+#'
+#' Spiroergometric data as imported by \code{\link{spiro}} will often come in
+#' large \code{data.frame}s. When knitting a R-Markdown document these will
+#' normally be printed in full size.
+#'
+#' This function provides a method for \code{data.frame}s of the class
+#' \code{spiro} to limit the displayed row number to \code{min} if it exceeds
+#' \code{max}. The number of hidden data rows will be printed below the
+#' \code{data.frame}.
+#'
+#' @param x A \code{data.frame} of the class \code{spiro} to be printed.
+#' @param min An integer, which sets the number of rows to which \code{x} will
+#'   be limited in printing if row number exceed \code{max}.
+#' @param max An integer, setting the maximal number of rows to be not cut to
+#'   \code{min} in printing.
+#' @param ... Passing of additional arguments to \code{knit_print.default()}.
+#'
+#' @importFrom knitr knit_print
+#' @export
+
+knit_print.spiro <- function(x,min = 10,max = 20,...) {
+  n <- nrow(x)
+  if (n<=max)
+    NextMethod(x,...)
+  else
+    x <- x[1:min,]
+  nr <- n-min
+  x[,4:ncol(x)] <- round(x[,4:ncol(x)],2)
+  c(NextMethod(x,...),sprintf("... with %s more rows", nr))
+}
