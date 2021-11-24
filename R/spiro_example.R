271b4ce6c805e838efbd54e2cd6f05ff075b15b1
Simon Nolte <81624911+smnnlt@users.noreply.github.com>
2021-11-24 14:19:56 +0100

    Allow empty file argument in spiro_example()

--- R/spiro_example.R
+++ R/spiro_example.R
@@ -3,11 +3,26 @@
 #' \code{spiro_example} returns the file path for example data files within the
 #' \code{spiro} package.
 #'
-#' @param file Name of the file. Either "zan_gxt", "zan_ramp" or "hr_ramp.tcx".
+#' @param file Name of the file, either "zan_gxt", "zan_ramp" or "hr_ramp.tcx".
+#'   Leave the argument empty to get a vector with the paths of all example
+#'   files.
+#'
+#' @return A character vector with the absolute file path of the example
+#'   file(s).
 #'
 #' @examples
+#' # get path of a specific example data file
 #' spiro_example("zan_gxt")
+#'
+#' # get all paths of example data files
+#' spiro_example()
 #' @export
-spiro_example <- function(file) {
-  system.file("extdata", file, package = "spiro", mustWork = TRUE)
+spiro_example <- function(file = NULL) {
+  if (is.null(file)) {
+    path <- system.file("extdata", package = "spiro")
+    out <- list.files(root, full.names = TRUE)
+  } else {
+    out <- system.file("extdata", file, package = "spiro", mustWork = TRUE)
+  }
+  out
 }
